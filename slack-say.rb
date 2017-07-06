ddfd74a4a2e2a9a0b82418cf4985961978f185b3
Paul David <paul@denknerd.org>
2017-07-06 14:36:56 +1000

    Add example script to say stuff on Slack.

---
+++ slack-say.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'json'
+require 'highline/import'
+require 'slack-ruby-client'
+
+def configs
+  @configs ||= JSON.parse File.read(CONFIG)
+end
+
+CONFIG = File.dirname(__FILE__) + '/config.json'
+
+Slack.configure do |config|
+  config.token = configs['SLACK_API_TOKEN']
+end
+
+client = Slack::Web::Client.new
+
+if HighLine.agree("Send message as @lunchbot now? (type \"y\")")
+  client.chat_postMessage(channel: '#melbourne',
+                          text: "RANDOM MESSAGE",
+                          as_user: true)
+end
